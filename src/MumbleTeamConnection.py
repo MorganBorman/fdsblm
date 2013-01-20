3dd9b132345a4a2fbec1ea617e143bd68059ff34
MorganBorman <morgan.borman@gmail.com>
2013-01-20 14:43:57 -0800

    Fixed issue where commands are sent multiple times.

--- src/MumbleTeamConnection.py
+++ src/MumbleTeamConnection.py
@@ -7,23 +7,35 @@ class MumbleTeamConnection(object):
         self.connect()
         
     def connect(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.hostname, self.port))
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.hostname, self.port))
+        except socket.error:
+            pass
     
     def send(self, msg):
         tries = 0
-        totalsent = 0
-        msglen = len(msg)
-        while totalsent < msglen:
-            sent = self.socket.send(msg[totalsent:])
-            if sent == 0:
+        while tries < 3:
+            try:
+                totalsent = 0
+                msglen = len(msg)
+                while totalsent < msglen:
+                    print "sending:", msg[totalsent:]
+                    sent = self.socket.send(msg[totalsent:])
+                    print sent
+                    if sent == 0:
+                        raise socket.error("error")
+                        totalsent = 0
+                    totalsent += sent
+                return
+            except socket.error:
                 self.connect()
-                self.totalsent = 0
-                tries += 1
                 if tries > 3:
                     print "Failed to send message to mumble autoteam server."
                     return
-            totalsent += sent
+                else:
+                    print "Failed to send message to mumble autoteam server. trying again..."
+            tries += 1
     
     def changeteam(self, uid, server_name, team_name):
         self.send("changeteam {} {} {}\n".format(uid, server_name, team_name))
